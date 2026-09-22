5922c13a26bad0ea67aedac931a0606594e02e4e
Alfinaswar <alfin7aswar@gmail.com>
2026-09-22 08:34:54 +0700

    1

--- app/Models/HalamanSolusiDetail.php
+++ app/Models/HalamanSolusiDetail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class HalamanSolusidetail extends Model
+class HalamanSolusiDetail extends Model
 {
     use SoftDeletes;
 
